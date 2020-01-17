7dec7c5b73d3b71d5a43669734c1ba6385c1719b
DoLT <dolt@runsystem.net>
2020-01-17 17:52:40 +0700

    update

---
+++ src/stories/Button.stories.js
@@ -0,0 +1,9 @@
+import React from "react";
+import Button from "../components/Button";
+
+export default {
+  title: "Button hehe",
+  component: Button
+};
+
+export const ButtonHehe = () => <Button />;
